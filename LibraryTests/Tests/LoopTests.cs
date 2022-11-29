faf6296d2d5de144d445e367c2b4c3d27df4ec94
James Craig <craig.jamesa@gmail.com>
2022-11-29 11:45:27 -0500

    - Update for loop tests.

--- LibraryTests/Tests/LoopTests.cs
+++ LibraryTests/Tests/LoopTests.cs
@@ -1,5 +1,8 @@
 ﻿using BenchmarkDotNet.Attributes;
 using LibraryTests.Tests.BaseClasses;
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace LibraryTests.Tests
 {
@@ -10,20 +13,29 @@ namespace LibraryTests.Tests
         [Benchmark(Description = "Foreach loop", Baseline = true)]
         public void ForEachLoop()
         {
-            foreach (var Item in TestData) { }
+            foreach (var Item in TestData)
+            {
+                HolderMethod(Item);
+            }
         }
 
         [Benchmark(Description = "For loop")]
         public void ForLoop()
         {
-            for (int x = 0; x < TestData.Length; ++x) { }
+            for (int x = 0; x < TestData.Length; ++x)
+            {
+                HolderMethod(TestData[x]);
+            }
         }
 
         [Benchmark(Description = "For loop, local length")]
         public void ForLoopLocalLength()
         {
             int Length = TestData.Length;
-            for (int x = 0; x < Length; ++x) { }
+            for (int x = 0; x < Length; ++x)
+            {
+                HolderMethod(TestData[x]);
+            }
         }
 
         [GlobalSetup]
@@ -31,5 +43,29 @@ namespace LibraryTests.Tests
         {
             TestData = new int[Count];
         }
+
+        [Benchmark(Description = "For loop, using span")]
+        public void SpanFor()
+        {
+            Span<int> ArraySpan = TestData;
+            for (int x = 0; x < ArraySpan.Length; ++x)
+            {
+                HolderMethod(ArraySpan[x]);
+            }
+        }
+
+        [Benchmark(Description = "For loop, using span and unsafe")]
+        public void UnsafeSpanFor()
+        {
+            Span<int> ArraySpan = TestData;
+            ref var SpanRef = ref MemoryMarshal.GetReference(ArraySpan);
+            for (int x = 0; x < ArraySpan.Length; ++x)
+            {
+                HolderMethod(Unsafe.Add(ref SpanRef, x));
+            }
+        }
+
+        private void HolderMethod(int x)
+        { }
     }
 }
