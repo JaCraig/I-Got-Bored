e62e0f2e489c818641f8180d01775685e526f72b
James Craig <craig.jamesa@gmail.com>
2024-02-18 12:23:07 -0500

    chore: Updating version of .Net to 8.0
    * Package updates to latest versions.

--- ResultBuilder/Program.cs
+++ ResultBuilder/Program.cs
@@ -7,26 +7,26 @@ using System.Text.RegularExpressions;
 
 namespace ResultBuilder
 {
-    internal class Program
+    internal static class Program
     {
         private static Regex HeaderData { get; } = new Regex(@"```(?<Header>[\s\S]*)```(?<Table>[\s\S]*)", RegexOptions.Multiline | RegexOptions.IgnoreCase);
 
         private static void Main(string[] args)
         {
-            new ServiceCollection().AddCanisterModules();
-            StringBuilder Builder = new StringBuilder("This is the repository for testing various speed improvements for Structure.Sketching and other libs. The results can be found below the conclussions section.\n\n# Conclussions\n\n# Results\n\n");
-            StringBuilder TableBuilder = new StringBuilder();
+            _ = new ServiceCollection().AddCanisterModules();
+            var Builder = new StringBuilder("This is a repository for testing various speed improvements for my various libraries and to test out general improvements between versions of .Net. The results can be found below the conclussions section.\n\n# Conclussions\n\n# Results\n\n");
+            var TableBuilder = new StringBuilder();
             var FinalFile = new FileInfo("../../../../README.md");
             var Header = "";
-            foreach (var File in new DirectoryInfo("../../../../LibraryTests/bin/Release/net5.0/BenchmarkDotNet.Artifacts/results").EnumerateFiles("*-github.md").OrderBy(x => x.Name))
+            foreach (FileCurator.Interfaces.IFile File in new DirectoryInfo("../../../../LibraryTests/bin/Release/net8.0/BenchmarkDotNet.Artifacts/results").EnumerateFiles("*-github.md").OrderBy(x => x.Name))
             {
                 var Data = File.Read();
                 var Table = HeaderData.Match(Data).Groups["Table"].Value;
                 Header = HeaderData.Match(Data).Groups["Header"].Value;
-                TableBuilder.AppendLine($"**{SplitCamelCase(File.Name)}**").AppendLine().AppendLine(Table).AppendLine("----------");
+                _ = TableBuilder.AppendLine($"**{SplitCamelCase(File.Name)}**").AppendLine().AppendLine(Table).AppendLine("----------");
             }
-            Builder.AppendLine($"```{Header}```").AppendLine(TableBuilder.ToString());
-            FinalFile.Write(Builder.ToString());
+            _ = Builder.AppendLine($"```{Header}```").AppendLine(TableBuilder.ToString());
+            _ = FinalFile.Write(Builder.ToString());
         }
 
         /// <summary>
@@ -34,7 +34,7 @@ namespace ResultBuilder
         /// </summary>
         /// <param name="input">The input.</param>
         /// <returns>Splits the camel case names</returns>
-        private static string SplitCamelCase(string? input)
+        private static string SplitCamelCase(string input)
         {
             input = input.Replace("LibraryTests.Tests.", "").Replace("-report-github.md", "");
             return input?.AddSpaces() ?? "";
